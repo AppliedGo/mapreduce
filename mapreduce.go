b4541ed16c51f717cc6db10a59d812b2e855c40c
Christoph Berger <christophberger@users.noreply.github.com>
2016-11-24 12:53:17 +0100

    Rewrote the first section: Included code, shortened the text.

--- mapreduce.go
+++ mapreduce.go
@@ -33,16 +33,69 @@ How Google tackled the problem of processing enormous amounts of data, and how y
 
 ## Map and Reduce
 
-Originally, `map` and `reduce` are two functions often seen in functional programming languages. The first applies a function to a list of objects, producing a list of results. The second reduces this list to a single output.
+This is going to be a boring article about two boring functions, `map()` and `reduce()`. Here is the story:
 
-This does not sound too fancy, except that these two functions do nothing less than making `for` loops obsolete. Furthermore, if the mapped function calls do not interact with each other, they can be trivially executed concurrently. That is, split up your input list into *n* pieces and pass them to *n* independently running mappers. On hardware that supports parallelization, the `map` operation suddenly gets multiple times faster! And harware parallelization can happen at different scales (think "multi-core CPU", "multi-CPU node", "multi-node rack", "multi-rack datacenter").
+You have a list with elements of type, say, string.
+
+```go
+var list []string
+```
+
+You have a function that takes an int and produces a string.
+
+```go
+func process(n int) (s string) {
+	return len(s)
+}
+```
+
+Now you define a function called `map()` that takes this function and applies it to each of the elements in the list and returns a list of all results.
+
+```go
+func map(list []string, fn func(string)int) []int {
+	res := make([]string, len(list))
+	for i, elem := range list {
+		res[i] = fn(elem)
+	}
+	return res
+}
+```
+
+Finally, you define another function `reduce()` that takes the result list and boils it down to a single result..
+
+```go
+func reduce(list []int) (res int) {
+	for _, elem := range list {
+		res += elem
+	}
+	return res
+}
+
+That's it. End of the story. Pretty boring, eh?
+
+
+## But wait! ...
+
+... what's this?
+
+**Looks like we just abstracted away the concept of `for` loops!**
+
+What does this buy us?
+
+First, no more one-off index errors.
+
+Second, and more importantly, if the mapped function `fn` does not depend on previous results, it can be trivially called in a concurrent manner.
+
+How to do this? Simple: Split the list into *n* pieces and pass them to *n* independently running mappers. Next, have the mappers run on separate CPU cores, or even on separate CPU's.
+
+Imagine the speedup you'll get.
 
 > Lemme repeat that. By abstracting away the very concept of looping, you can implement looping any way you want, including implementing it in a way that scales nicely with extra hardware.
 >
 > Joel Spolsky, [Can Your Programming Language Do This?](http://www.joelonsoftware.com/items/2006/08/01.html) (2006)
 
 
-## The next step: MapReduce
+## From map and reduce to MapReduce
 
 
 
